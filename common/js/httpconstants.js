0e2bcda631f13f06bb66d6b84ceb6a49e7a30abd
samutay <samutay@yahoo.com>
2013-12-23 09:32:51 -0500

    initial pages

---
+++ common/js/httpconstants.js
@@ -0,0 +1,70 @@
+var API_APPKEY = "dia";
+var API_SIGNATURE = "lwe";
+var API_TIMESTAMP = "wtj";
+
+var PARAM_ACTION = "qxh";
+var PARAM_AUCTIONUID = "uxa";
+var PARAM_BIDDERNUMBER = "ode";
+var PARAM_CATEGORYUID = "zfc";
+var PARAM_DEVICEID = "roc";
+var PARAM_DEVICETYPE = "kos";
+var PARAM_END = "aib";
+var PARAM_FIRSTNAME = "vxh";
+var PARAM_ITEMNUMBER = "nje";
+var PARAM_ITEMUID = "wqg";
+var PARAM_LASTNAME = "gzp";
+var PARAM_LENGTH = "mfu";
+var PARAM_NAME = "cng";
+var PARAM_PASSWORD = "mvk";
+var PARAM_PRICE = "sfe";
+var PARAM_START = "ymd";
+var PARAM_SESSIONID = "jnf";
+var PARAM_USERUID = "oev";
+var PARAM_DESCRIPTION = "zip";
+var PARAM_CATEGORY = "few";
+var PARAM_SELLER = "eye";
+var PARAM_VALPRICE = "iea";
+var PARAM_MINPRICE = "bju";
+var PARAM_INCPRICE = "nyv";
+var PARAM_CALLBACK = "callback";
+var PARAM_SORT = "fgd";
+var PARAM_DIR = "fed";
+
+var ACTION_AUCTIONLIST = "ycs";
+var ACTION_AUCTIONLISTCNT = "gkh";
+var ACTION_AUCTIONEDIT = "gxk";
+var ACTION_AUCTIONDELETE = "sbt";
+
+var ACTION_CATEGORYLIST = "gje";
+var ACTION_CATEGORYLISTCNT = "ogc";
+var ACTION_CATEGORYEDIT = "zmt";
+var ACTION_CATEGORYDELETE = "fxr";
+
+var ACTION_ITEMLIST = "bex";
+var ACTION_ITEMLISTCNT = "pgp";
+var ACTION_ITEMEDIT = "vku";
+var ACTION_ITEMDELETE = "orb";
+var ACTION_ITEMUPDATES = "mmm";
+var ACTION_ITEMIMPORT = "uty";
+var ACTION_MYWATCHITEMLIST = "rfr";
+var ACTION_MYBIDITEMLIST = "jgj";
+
+var ACTION_USERLIST = "kyl";
+var ACTION_USERLISTCNT = "dit";
+var ACTION_USEREDIT = "nhe";
+var ACTION_USERDELETE = "ake";
+var ACTION_USERIMPORT = "hgy";
+
+var ACTION_BID = "say";
+var ACTION_WATCH = "ros";
+var ACTION_FUNDACAUSE = "gth";
+
+var ACTION_SIGNIN = "iuy";
+var ACTION_SIGNOUT = "tfg";
+
+var ACTION_GENERATEQRCODE = "iij";
+
+var ACTION_DEVICELIST = "nwl";
+var ACTION_DEVICELISTCNT = "gqu";
+var ACTION_REGISTERDEVICE = "olk";
+var ACTION_DEVICEMESSAGE = "vdf";
